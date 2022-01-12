5e383df59900c830e012df0fda72d7f0ee302055
vanlam0110 <97009313+vanlam0110@users.noreply.github.com>
2022-01-12 16:30:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    introTitle: 'Tặng em',
+    introDesc: `ẤN VÔ HÌNH TRÁI TYM`,
+    btnIntro: '❤️',
+    title: 'HAPPY BIRTHDAY 🎂',
+    desc: 'Sau này bé cưới anh hong ?',
+    btnYes: 'Cưới chớ ❤️',
+    btnNo: 'Hong cưới',
+    question:'Iu anh hong 😘',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    mess: 'Dù câu trả lời thế nào anh cũng biết em iu anh mà🥰. IU IU 😘😘',
+    messDesc: 'Bây giờ muốn nói gì với anh thì hình lá thư ở dưới',
+    btnAccept: '✉️',
+    messLink: 'https://www.facebook.com/profile.php?id=100062874783772' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
