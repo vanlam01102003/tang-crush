7fc155f9533166859ca413fb7c492d0b0d0671ba
vanlam0110 <97009313+vanlam0110@users.noreply.github.com>
2022-01-13 16:52:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ const CONFIG = {
     btnNo: 'Hong cưới 🙄',
     question:'Yêu anh hong ❤️',
     btnReply: 'Gửi',
-    reply: 'Yêu thì yêu mà không yêu thì yêu ❤️',
-    mess: 'Dù câu trả lời thế nào anh cũng biết bé yeu anh mà🥰.YÊU YÊU 😘',
+    reply: '',
+    mess: 'Dù câu trả lời thế nào anh cũng biết bé yêu anh mà🥰.YÊU YÊU 😘',
     messDesc: 'Bây giờ muốn nói gì với anh thì thì bấm vô lá thư ✉️',
     btnAccept: '✉️',
     messLink: 'https://www.facebook.com/profile.php?id=100062874783772' //link mess của các bạn. VD: https://m.me/nam.nodemy
